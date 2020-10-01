43e2f836e91370465e376a3dda216605e4c90f2f
Devonshire <www.devonshire99@mail.ru>
2020-10-01 20:30:57 +0300

    first commit

--- app/src/main/java/anton/ryaby_belstu/stpmslab_01/MainActivity.java
+++ app/src/main/java/anton/ryaby_belstu/stpmslab_01/MainActivity.java
@@ -3,6 +3,10 @@ package anton.ryaby_belstu.stpmslab_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import anton.ryaby_belstu.stpmslab_01.MyPackage.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("STPMSLab_001", "Counter " + count);
+        }
+        displayText();
     }
+
+    private void displayText() {
+        //TODO text function
+        TextFunction textFunction = new TextFunction();
+        TextView textView = findViewById(R.id.tv_text);
+        textView.setText(textFunction.getValue());
+    }
+
 }
---
+++ app/src/main/java/anton/ryaby_belstu/stpmslab_01/MyPackage/TextFunction.java
@@ -0,0 +1,7 @@
+package anton.ryaby_belstu.stpmslab_01.MyPackage;
+
+public class TextFunction {
+    public String getValue() {
+        return "Hello from First project";
+    }
+}
